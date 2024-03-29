20d51b40b508eae3eb60e50cc8ae01ab562fe6f7
MRKittipot <kittipoj2545@gmail.com>
2024-03-29 15:42:56 +0700

    update 15.42

--- emerweb/src/Page/Home.js
+++ emerweb/src/Page/Home.js
@@ -1,4 +1,7 @@
 import "../css/Home.css"
+import {FaRegNewspaper} from "react-icons/fa6"
+import { CgProfile } from "react-icons/cg";
+import { BsClipboard2Data } from "react-icons/bs";
 
 function Home(){
     return(
@@ -12,12 +15,21 @@ function Home(){
                 <h1 className="home-title">EMER Management</h1>
                 <div className="card-list">
                     <div className="card">
+                    <div className="IconBg">
+                        <CgProfile className="Icon" size={90}/>
+                    </div>
                         <label>Proflie</label>
                     </div>
                     <div className="card">
+                        <div className="IconBg">
+                            <BsClipboard2Data className="Icon" size={90}/>
+                        </div>
                         <label>Dashboard</label>
                     </div>
                     <div className="card">
+                        <div className="IconBg">
+                            <FaRegNewspaper className="Icon" size={90}/>
+                        </div>
                         <label>Blog</label>
                     </div>
                 </div>
